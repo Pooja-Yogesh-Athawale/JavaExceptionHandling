c628860bb8679e9d1cc1f488dcd3d9add3e9d61e
Pooja <athawale.pooja2015@gmail.com>
2022-09-01 22:11:50 -0700

    Initial commit

---
+++ src/exceptionExample/Phone.java
@@ -0,0 +1,30 @@
+package exceptionExample;
+
+public class Phone {
+    private final String phoneType;
+    private final String phoneNumber;
+
+    public Phone(String phoneType, String phoneNumber) {
+        super();
+
+        if (phoneType == null || phoneNumber == null) {
+            throw new IllegalArgumentException("The type and number cannot be null");
+        }
+        this.phoneType = phoneType;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneType() {
+        return phoneType;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return phoneType + " " + phoneNumber;
+    }
+
+}
